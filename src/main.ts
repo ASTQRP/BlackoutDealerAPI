0179390d932b4d384093ca0f01212f5ec7a14e68
Jackecx02 <jacobcuevas181@gmail.com>
2022-05-14 20:06:54 -0400

    Deployable app

--- src/main.ts
+++ src/main.ts
@@ -4,28 +4,23 @@ import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  // const port = process.env.PORT || 3000;
-
-  // const app = await NestFactory.create(AppModule);
-
-  // app.use(bodyParser.json({ limit: '50mb' }));
-  // app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  // app.enableCors();
-
-  // const config = new DocumentBuilder()
-  //   .setTitle('Dealer API example')
-  //   .setDescription('The Dealer API description')
-  //   .setVersion('1.0')
-  //   .build();
-
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
-
-  // await app.listen(port);
-  // console.clear();
-  // console.log(`App listening in port: ${port}`);
-  const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+
+  const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Dealer API example')
+    .setDescription('The Dealer API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 }
 
